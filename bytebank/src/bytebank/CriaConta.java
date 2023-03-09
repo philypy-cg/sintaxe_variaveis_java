c789743b1d330f3557f5ad087b408845e5468fd8
Philypy Gomes <phcg.dev@gmail.com>
2023-03-09 19:39:05 -0300

    aut orient obj

--- bytebank/src/bytebank/CriaConta.java
+++ bytebank/src/bytebank/CriaConta.java
@@ -3,7 +3,7 @@ package bytebank;
 public class CriaConta {
 	public static void main(String[] args) {
 		Conta primeiraConta = new Conta();
-		primeiraConta.saldo = 200;
+		primeiraConta.saldo = 200 + 145;
 		System.out.println(primeiraConta.saldo);
 	}
 
