52546925ffde3aa8070821120b0fa086ad365260
Philypy Gomes <phcg.dev@gmail.com>
2023-03-16 17:57:31 -0300

    classe teste

---
+++ bytebank_herdado/src/bytebank_herdado/Designer.java
@@ -0,0 +1,10 @@
+package bytebank_herdado;
+
+public class Designer extends Funcionario {
+
+	public double getBonificacao() {
+		System.out.println("Chamando o metodo de bonificacao do Designer");
+		return 200;
+	}
+
+}
